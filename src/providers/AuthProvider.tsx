a86761ffc61f1ac57e24a3b2182a7ecb6132ee43
markostupar98 <markostuparets@gmail.com>
2024-05-22 14:51:16 +0200

    added sign up functions, supabase , auth, query, and cart provider

---
+++ src/providers/AuthProvider.tsx
@@ -0,0 +1,64 @@
+import { supabase } from "../lib/supabase";
+import { Session } from "@supabase/supabase-js";
+import {
+  createContext,
+  PropsWithChildren,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import { ActivityIndicator } from "react-native";
+
+// Defining type
+type AuthData = {
+  session: Session | null;
+  loading: boolean;
+  profile: any;
+  isAdmin: boolean;
+};
+
+const AuthContext = createContext<AuthData>({
+  session: null,
+  loading: true,
+  profile: null,
+  isAdmin: false,
+});
+
+export default function AuthProvider({ children }: PropsWithChildren) {
+  const [session, setSession] = useState<Session | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [profile, setProfile] = useState(null);
+
+  useEffect(() => {
+    const getSession = async () => {
+      const {
+        data: { session },
+      } = await supabase.auth.getSession();
+      setSession(session);
+      // Group based auth
+      if (session) {
+        const { data } = await supabase
+          .from("profiles")
+          .select("*")
+          .eq("id", session.user.id)
+          .single();
+        setProfile(data || null);
+      }
+      setLoading(false);
+    };
+    getSession();
+    // On change session
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+  }, []);
+  return (
+    <AuthContext.Provider
+      value={{ session, loading, profile, isAdmin: profile?.group === "ADMIN" }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export const useAuth = () => useContext(AuthContext);
--- src/screens/auth/SignUpScreen.tsx
+++ src/screens/auth/SignUpScreen.tsx
@@ -1,77 +1,130 @@
-import { View, Text, TextInput, Pressable } from "react-native";
+import { View, Text, TextInput, Pressable, Alert } from "react-native";
 import React, { useState } from "react";
 import Header from "../../components/Header";
+import { supabase } from "../../lib/supabase";
 import * as Animatable from "react-native-animatable";
 import AntDesign from "@expo/vector-icons/AntDesign";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 import { Button } from "@rneui/themed";
+import { Link, useNavigation } from "expo-router";
 
 const SignUpScreen = () => {
+  const navigation = useNavigation();
+
+  const [username, setUsername] = useState("");
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const toggleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  // Supabase sign up
+  async function signUpWithEmail() {
+    setLoading(true);
+
+    const { data, error } = await supabase.auth.signUp({
+      email,
+      password,
+      options: {
+        data: {
+          fullName,
+          username,
+        },
+      },
+    });
+
+    setLoading(false);
+
+    if (error) {
+      Alert.alert("Error", error.message);
+    } else {
+      Alert.alert(
+        "Success",
+        "Please check your email for the confirmation link."
+      );
+      navigation.navigate("SignInScreen");
+    }
+  }
+
   return (
     <View className="flex-1">
-    <Header title="Sign Up" type="back" />
-    <View className="p-8 mt-3 items-center">
-      <Text className="text-xl text-neutral-600">
-        Create your account
-      </Text>
-    </View>
-    <View className="items-center justify-center mt-2">
-    </View>
-    <View className="mt-8">
-    <View>
-        <TextInput
-          placeholder="Username"
-          className="border border-neutral-300 mb-8 mx-5 rounded-lg h-10 p-2"
-        />
+      <Header title="Sign Up" type="back" />
+      <View className="p-8 mt-3 items-center">
+        <Text className="text-xl text-neutral-600">Create your account</Text>
       </View>
-      <View>
-        <TextInput
-          placeholder="Full Name"
-          className="border border-neutral-300 mb-8 mx-5 rounded-lg h-10 p-2"
-        />
-      </View>
-      <View>
-        <TextInput
-          placeholder="Email"
-          className="border border-neutral-300 mb-8 mx-5 rounded-lg h-10 p-2"
-        />
-      </View>
-      <View className="border mx-5 border-neutral-300  flex-row justify-between items-center mb-5  rounded-lg">
-        <Animatable.View>
-          <AntDesign name="lock1" size={24} color="black" />
-        </Animatable.View>
-        <TextInput placeholder="Password" className="h-10" />
-
-        <Animatable.View>
-          <MaterialIcons
-            name="visibility-off"
-            size={24}
-            color="black"
-            style={{ marginRight: 5 }}
+      <View className="items-center justify-center mt-2"></View>
+      <View className="mt-8">
+        <View>
+          <TextInput
+            value={username}
+            onChangeText={setUsername}
+            placeholder="Username"
+            className="border border-neutral-300 mb-8 mx-5 rounded-lg h-10 p-2"
           />
-        </Animatable.View>
+        </View>
+        <View>
+          <TextInput
+            value={fullName}
+            onChangeText={setFullName}
+            placeholder="Full Name"
+            className="border border-neutral-300 mb-8 mx-5 rounded-lg h-10 p-2"
+          />
+        </View>
+        <View>
+          <TextInput
+            value={email}
+            onChangeText={setEmail}
+            placeholder="Email"
+            className="border border-neutral-300 mb-8 mx-5 rounded-lg h-10 p-2"
+          />
+        </View>
+        <View className="border mx-5 border-neutral-300  flex-row justify-between items-center mb-5  rounded-lg">
+          <Animatable.View>
+            <AntDesign name="lock1" size={24} color="black" />
+          </Animatable.View>
+          <TextInput
+            secureTextEntry={!showPassword}
+            value={password}
+            onChangeText={setPassword}
+            placeholder="Password"
+            className="h-10"
+          />
+
+          <Animatable.View>
+            <MaterialIcons
+              name={`${showPassword ? "visibility" : "visibility-off"}`}
+              size={24}
+              color="black"
+              style={{ marginRight: 5 }}
+              onPress={toggleShowPassword}
+            />
+          </Animatable.View>
+        </View>
+      </View>
+      <View className="w-90 mx-7 my-2">
+        <Button
+          disabled={loading}
+          title={"Register"}
+          buttonStyle={{
+            borderRadius: 30,
+            backgroundColor: "rgba(111, 202, 186, 1)",
+          }}
+          onPress={signUpWithEmail}
+        />
+      </View>
+      <View className="items-center my-3">
+        <Text className="text-sm text-neutral-400">
+          Already have an account
+        </Text>
+        <Pressable className="my-2">
+          <Text className="font-bold underline">Sign In</Text>
+        </Pressable>
       </View>
     </View>
-    <View className="w-90 mx-7 my-2">
-      <Button
-        title={"Register"}
-        buttonStyle={{
-          borderRadius: 30,
-          backgroundColor: "rgba(111, 202, 186, 1)",
-        }}
-        onPress={()=>{navigation.navigate('HomeScreen')}}
-      />
-    </View>
-    <View className="items-center my-3">
-      <Text className="text-sm text-neutral-400">Already have an account</Text>
-      <View className="my-2">
-        <Text className="font-bold underline">Sign In</Text>
-      </View>
-    </View>
-  
-    
-    
-  </View>
   );
 };
 
