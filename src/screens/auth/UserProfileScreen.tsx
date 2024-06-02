b0f8cf7c597001a065f1eca818a2a3cbd1fb9c44
markostupar98 <markostuparets@gmail.com>
2024-06-02 19:02:55 +0200

    fixing user profile screen, updatng location and sign out

--- src/screens/auth/UserProfileScreen.tsx
+++ src/screens/auth/UserProfileScreen.tsx
@@ -4,45 +4,47 @@ import {
   Image,
   TouchableOpacity,
   Alert,
-  StyleSheet,
   ActivityIndicator,
 } from "react-native";
 import React, { useState, useEffect } from "react";
-import { supabase } from "../../lib/supabase";
 import { useNavigation } from "@react-navigation/native";
 import { Button } from "@rneui/themed";
 import Header from "../../components/Header";
 import MapView, { Marker } from "react-native-maps";
 import Background from "../../components/Background";
+import { fetchUserProfile, updateUserProfile } from "../../services/userService";
+import { useSelector, useDispatch } from "react-redux";
+import { clearUser } from "../../store/slice/userSlice";
 
 const UserProfileScreen = () => {
   const navigation = useNavigation();
+  const userId = useSelector((state) => state.user.id); 
   const [user, setUser] = useState(null);
+  const dispatch = useDispatch();
   const [location, setLocation] = useState(null);
   const [address, setAddress] = useState(null);
 
   // Get user
   useEffect(() => {
     const fetchUser = async () => {
-      const { data, error } = await supabase.auth.getUser();
+      const { profile, error } = await fetchUserProfile(userId);
       if (error) {
         console.error(error);
       } else {
-        setUser(data.user);
+        setUser(profile);
+        if (profile.latitude && profile.longitude) {
+          setLocation({ latitude: profile.latitude, longitude: profile.longitude });
+        }
+        setAddress(profile.address);
       }
     };
     fetchUser();
-  }, []);
+  }, [userId]);
 
-  // Sign out
-
-  const signOut = async () => {
-    const { error } = await supabase.auth.signOut();
-    if (error) {
-      Alert.alert("Error", error.message);
-    } else {
-      navigation.navigate("SignInScreen");
-    }
+   // Sign out
+   const signOut = () => {
+    dispatch(clearUser());
+    navigation.navigate("SignInScreen");
   };
 
   if (!user) {
@@ -53,6 +55,7 @@ const UserProfileScreen = () => {
     );
   }
 
+  // Location changing based on map press
   const handleMapPress = async (event) => {
     const { latitude, longitude } = event.nativeEvent.coordinate;
     setLocation({ latitude, longitude });
@@ -68,21 +71,20 @@ const UserProfileScreen = () => {
 
   const saveLocation = async () => {
     if (user) {
-      const { error } = await supabase
-        .from("profiles")
-        .update({
-          latitude: location.latitude,
-          longitude: location.longitude,
-          address: address,
-        })
-        .eq("id", user.id);
+      const { profile, error } = await updateUserProfile(user.id, {
+        latitude: location.latitude,
+        longitude: location.longitude,
+        address: address,
+      });
       if (error) {
         Alert.alert("Error", error.message);
       } else {
         Alert.alert("Success", "Location updated successfully!");
+        setUser(profile);
       }
     }
   };
+
   return (
     <Background> 
     <View className="flex-1">
@@ -92,8 +94,8 @@ const UserProfileScreen = () => {
           source={require("../../../assets/deliveryguy.jpeg")}
           className="w-17 h-17 rounded-full"
         />
-        <Text className="mt-2 text-base">Full Name: {user.user_metadata.fullName}</Text>
-        <Text className="mt-2 text-neutral-500">Email: {user.email}</Text>
+        <Text className="mt-2 text-base">Full Name: {user.fullName}</Text>
+        {/* <Text className="mt-2 text-neutral-500">Email: {user.email}</Text> */}
         <View className="w-60 h-60 mt-3 ">
           <MapView
             className="flex-1 rounded-2xl"
